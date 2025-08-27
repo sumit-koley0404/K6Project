f366e014380d8fff04c9c6ff6b0f4d2350d86a3a
sumit-koley0404 <sumitkoley180@gmail.com>
2025-08-27 21:41:44 +0530

    message update

--- loadTest.js
+++ loadTest.js
@@ -5,7 +5,7 @@ export const options = {
     stages: [
         //Ramp-up from 1 to 10 users over 5s
         {duration: '5s', target: 10},
-        //Stay at 10 users for 15s
+        // Ramp-up from 10 to 30 users and stay for 15s
         {duration: '15s', target: 30},
         //Ramp-down to 0 users over 5s
         {duration: '5s', target: 0}
