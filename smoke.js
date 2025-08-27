7c9c4420fa65762caa7be7a095199d19d05e7e14
sumit-koley0404 <sumitkoley180@gmail.com>
2025-08-27 21:14:37 +0530

    smoke test

---
+++ smoke.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options ={
+    vus: 1,
+    duration: '30s',
+}
+
+export default function(){
+    http.get('http://test.k6.io');
+    sleep(1);
+    http.get('http://test.k6.io/contacts.php');
+    sleep(1);
+    http.get('http://test.k6.io/news.php');
+    sleep(1);
+}
