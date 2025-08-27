15db33d00ad809071afebe1f47810d894c0328b1
sumit-koley0404 <sumitkoley180@gmail.com>
2025-08-27 21:39:00 +0530

    load Test

---
+++ loadTest.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options = {
+    stages: [
+        //Ramp-up from 1 to 10 users over 5s
+        {duration: '5s', target: 10},
+        //Stay at 10 users for 15s
+        {duration: '15s', target: 30},
+        //Ramp-down to 0 users over 5s
+        {duration: '5s', target: 0}
+    ]
+}
+
+export default function(){
+    http.get('http://test.k6.io');
+    sleep(1);
+    http.get('http://test.k6.io/contacts.php');
+    sleep(2);
+    http.get('http://test.k6.io/news.php');
+    sleep(1);
+}
